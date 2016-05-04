c757462005a13fd4cefd63ddb33350e853f41744
MS <noreply@reply.not>
2016-05-04 17:06:49 +0200

    Started on workspace triple split feature

--- src/app/components/workspace/workspace.ts
+++ src/app/components/workspace/workspace.ts
@@ -1,4 +1,60 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild, Renderer, Inject} from "@angular/core";
+
+/**
+ * Simple split pane.
+ */
+@Component({
+    selector: 'double-split',
+    template: require('./doublesplit.html'),
+    styles: [require('./splitpane.scss')]
+})
+class DoubleSplit {
+    @ViewChild('divider') middle: ElementRef;
+}
+
+/**
+ * Three column split pane.
+ */
+@Component({
+    selector: 'triple-split',
+    template: require('./triplesplit.html'),
+    styles: [require('./splitpane.scss')]
+})
+class TripleSplit {
+
+    @ViewChild('leftContent') leftContent: ElementRef;
+    @ViewChild('leftDivider') leftDiv: ElementRef;
+    @ViewChild('rightDivider') rightDiv: ElementRef;
+    @ViewChild('rightContent') rightContent: ElementRef;
+    @ViewChild('centerContent') center: ElementRef;
+    
+    private centerMin = 20;
+
+    /**
+     *
+     * @param left
+     * @param right
+     */
+    readjust(left: number, right: number) {
+        const renderer = this.renderer;
+        left = (left < 0)? 0 : (left > 100) ? 100 : 0;
+        right = (right < 0)? 0 : (right > 100) ? 100 : 0;
+        let adjLeft = Math.min(left, right - this.centerMin);
+        let adjRight = Math.max(left + this.centerMin, right);
+        let leftStyle = `${adjLeft}%`;
+        let cenStyle = `${100 - adjLeft - adjRight}%`;
+        let rightStyle = `${adjRight}%`;
+        renderer.setElementStyle(this.leftContent, 'width', leftStyle);
+        renderer.setElementStyle(this.leftDiv,'left', leftStyle);
+        renderer.setElementStyle(this.center, 'left', leftStyle);
+        renderer.setElementStyle(this.center, 'width', cenStyle);
+        renderer.setElementStyle(this.rightDiv, 'left', rightStyle);
+        renderer.setElementStyle(this.rightContent, 'left', rightStyle);
+        renderer.setElementStyle(this.rightContent, 'width', `${100 - adjRight}%`)
+    }
+
+    constructor(@Inject(Renderer) private renderer: Renderer) {}
+}
 
 /**
  * Workspace component.
@@ -6,28 +62,8 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'workspace',
     template: require('./workspace.html'),
-    styles: [require('./workspace.scss)')],
     directives: [DoubleSplit, TripleSplit],
 })
 export default class Workspace {
     slim: boolean = true;
-}
-    
-    
-@Component({
-    selector: 'double-split',
-    template: require('./doublesplit.html'),
-    styles: [require('./splitpane.scss')]
-})
-class DoubleSplit {
-    
-}
-
-@Component({
-    selector: 'triple-split',
-    template: require('./triplesplit.html'),
-    styles: [require('./splitpane.scss')]
-})
-class TripleSplit {
-    
 }
