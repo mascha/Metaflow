89dae694d3bb226b0200a9a35530bcdd79f6aa38
MS <noreply@reply.not>
2016-05-04 13:48:04 +0200

    moved splitpane to workspace

--- src/app/components/workspace/workspace.ts
+++ src/app/components/workspace/workspace.ts
@@ -1,17 +1,18 @@
-/**
- * Created by Martin Schade on 25.04.2016.
- */
-
 import {Component} from "angular2/core";
+
+/**
+ * 
+ */
 @Component({
     selector: 'workspace',
     template: require('./workspace.html'),
     styles: [require('./workspace.scss)')]
 })
 export default class Workspace {
-    
+    slim: boolean = true;
 }
-
+    
+    
 @Component({
     selector: 'double-split',
     template: require('./doublesplit.html')
