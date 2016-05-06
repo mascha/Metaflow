52b48a4a2bb4cad52e96c8939fb522bd3173f242
MS <noreply@reply.not>
2016-05-06 17:08:24 +0200

    Implementing renderer, layer

--- src/app/components/platform/pixi.ts
+++ src/app/components/platform/pixi.ts
@@ -2,14 +2,55 @@
  * Copyright (C) Martin Schade, 2015-2016. All rights reserved.
  */
 
-import {Camera} from "../../common/camera";
+import {Camera, ICameraObserver} from "../../common/camera";
 
-class PixiCamera extends Camera {
+/**
+ * Provides a pan-zoom surface for Pixi.js.
+ *
+ * @author Martin Schade
+ * @since 1.0.0
+ */
+export class PixiCamera extends Camera {
 
-    protected translateWorldTo(tX:number, tY:number):void {
+    protected translateWorldTo(tX:number, tY:number) {
+        const position = this.graphics.position;
+        position.x = tX;
+        position.y = tY;
     }
 
-    protected scaleWorldTo(zoom:number):void {
+    protected scaleWorldTo(zoom:number) {
+        let scale = this.graphics.scale;
+        scale.x = zoom;
+        scale.y = zoom;
+    }
+
+    constructor(private graphics: PIXI.Graphics) {
+        super();
+    }
+}
+
+export function PixiCameraProvider(graphics: PIXI.Graphics): Camera {
+    return new PixiCamera(graphics);
+}
+
+export function PixiStageProvider(): any {
+    return new PIXI.Container();
+}
+
+export function PixiGraphicsProvider(): PIXI.Graphics {
+    return new PIXI.Graphics();
+}
+
+
+export class PixiLayer implements ICameraObserver {
+    
+    onViewResized():void {
+    }
+
+    onPanChanged(posX:number, posY:number):void {
+    }
+
+    onZoomChanged(zoom:number):void {
     }
 
 }
