ecd9606b94dfeb71c160345b0872ac7970c62bee
MS <noreply@reply.not>
2016-05-07 10:42:08 +0200

    Sidebar boilerplate

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,1 +1,9 @@
+import {Component} from '@angular/core';
 
+@Component({
+    selector: 'sidebar',
+    template: require('./sidebar.html'),
+    styles: [require('./sidebar.css')]
+})
+export default class Sidebar {
+}
