66706a3d4bd1e345e77c5e968c7270b817f6a710
MS <noreply@reply.not>
2016-05-07 19:48:37 +0200

    Added top navigation header

---
+++ src/app/components/headerbar/headerbar.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'header-bar',
+    styles: [require('./headerbar.scss')],
+    template: require('./headerbar.html')
+})
+export default class Headerbar {
+    
+}
