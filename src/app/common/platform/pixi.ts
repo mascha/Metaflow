e8aafa666dc2504ad39b98abc56dbe55b35cffaf
MS <noreply@reply.not>
2016-05-07 13:08:27 +0200

    Implemented pixi rederer

--- src/app/common/platform/pixi.ts
+++ src/app/common/platform/pixi.ts
@@ -3,8 +3,8 @@
  */
 
 import {Camera} from "../camera";
-import {ViewGroup} from "../viewmodel";
-import {IPlatformLayer} from "../renderer";
+import {ViewGroup, ViewItem, ViewVertex} from "../viewmodel";
+import {IPlatformLayer, IViewModelRenderer} from "../renderer";
 
 /**
  * Provides a pan-zoom surface for pixi.js.
@@ -88,4 +88,64 @@ export class PixiLayer implements IPlatformLayer {
         this.render = this.renderer.render;
         element.appendChild(this.renderer.view)
     }
+}
+
+
+/**
+ * Implements the pixi.js platform renderer.
+ *
+ * @author Martin Schade
+ * @since 1.0.0
+ */
+export class PixiRenderer implements IViewModelRenderer<any, any> {
+
+    renderItem(item:ViewItem):any {
+        return new PIXI.Graphics()
+            .lineStyle(4, 0xFF3300, 1)
+            .beginFill(0x66CCFF)
+            .drawRect(item.left, item.top, item.width, item.height)
+            .endFill();
+    }
+
+    renderGroup(group:ViewGroup, topLevel:boolean):any {
+        let root = new PIXI.Container();
+        root.width = group.width;
+        root.height = group.height;
+
+        if (!topLevel) {
+            root.position.set(group.left, group.top);
+        }
+
+        let label = new PIXI.Text(group.label);
+
+        let shape = new PIXI.Graphics();
+        shape.lineStyle(4, 0xFF3300, 1);
+        shape.drawRect(0, 0, root.width, root.height);
+
+        let content = new PIXI.Container();
+        let inner = group.scale;
+        content.scale.set(inner, inner);
+
+        root.addChild(label);
+        root.addChild(shape);
+        root.addChild(content);
+    }
+
+    renderTree(group:ViewGroup):any {
+        throw new Error('Not implemented yet');
+    }
+
+    attach(node:ViewVertex, group:ViewGroup) {
+        let child = node.visual;
+        if (!child) throw new Error('Node has no rendered visual');
+
+        let root = group.visual as PIXI.Container;
+        if (!root) throw new Error('Could not find renderer visual of the given group');
+
+        let content = root.children[1] as PIXI.Container;
+        if (!content) throw new Error('Could not find low level content container');
+
+        content.addChild(child);
+    }
+
 }
