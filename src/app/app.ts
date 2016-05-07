1ba85eb1b9999009923cb8d4407525ce28250b1f
MS <noreply@reply.not>
2016-05-07 22:45:17 +0200

    Added navigation to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {FORM_PROVIDERS} from '@angular/common';
+
 import Workspace from "./components/workspace/workspace";
+import Headerbar from "./components/headerbar/headerbar";
 
 import '../style/app.scss';
 
 /**
- * Metalogic Web Application
+ * Metalogic Web Application.
+ * @author Martin Schade
+ * @since 1.0.0
  */
 @Component({
   selector: 'app',
   providers: [...FORM_PROVIDERS],
-  directives: [...ROUTER_DIRECTIVES, Workspace],
+  directives: [...ROUTER_DIRECTIVES, Workspace, Headerbar],
   pipes: [],
   styles: [require('./app.scss')],
   template: require('./app.html')
