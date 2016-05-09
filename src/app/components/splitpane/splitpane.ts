9b6daf85fb384ae0c35fbfc594c4abafdc3366cc
triLithium <noreply@reply.not>
2016-05-09 10:54:17 +0200

    Style and theme changes

--- src/app/components/splitpane/splitpane.ts
+++ src/app/components/splitpane/splitpane.ts
@@ -15,17 +15,12 @@ export class DoubleSplit {
     @ViewChild('divider') left: ElementRef;
     @ViewChild('rightContent') rightContent: ElementRef;
     
-    private lastLeft = 20;
-    private lastRight = 80;
+    private lastLeft = 50;
 
     private moveHandler = (event: MouseEvent) => {
         HTML.block(event);
         let adjusted = Math.round(event.pageX/window.innerWidth*100);
-        if (this.left) {
-            this.readjust(adjusted, this.lastRight);
-        } else {
-            this.readjust(this.lastLeft, adjusted)
-        }
+        this.readjust(adjusted);
     };
 
     private upHandler = (event: MouseEvent) => {
@@ -45,35 +40,18 @@ export class DoubleSplit {
      * @param l
      * @param r
      */
-    readjust(l: number, r: number) {
+    readjust(l: number) {
         const renderer = this.renderer;
         let left = (l < 0)? 0 : (l > 100) ? 100 : l;
-        let right = (r < 0)? 0 : (r > 100) ? 100 : r;
-        let adjLeft = Math.min(left, right);
-        let adjRight = Math.max(left, right);
+        let adjLeft = Math.min(left, 100);
         let doLeft = Math.abs(this.lastLeft - adjLeft) > 0;
-        let doRight = Math.abs(this.lastRight - adjRight) > 0;
 
         if (doLeft) {
             let leftStyle = `${adjLeft}%`;
             renderer.setElementStyle(this.leftContent.nativeElement, 'width', leftStyle);
-            renderer.setElementStyle(this.leftDiv.nativeElement,'left', leftStyle);
-            renderer.setElementStyle(this.center.nativeElement, 'left', leftStyle);
+            renderer.setElementStyle(this.left.nativeElement,'left', leftStyle);
+            renderer.setElementStyle(this.rightContent.nativeElement, 'left', leftStyle);
             this.lastLeft = adjLeft;
-        }
-
-        if (doRight) {
-            let rightStyle = `${adjRight}%`;
-            renderer.setElementStyle(this.rightDiv.nativeElement, 'left', rightStyle);
-            renderer.setElementStyle(this.rightContent.nativeElement, 'left', rightStyle);
-            this.lastRight = adjRight;
-        }
-
-        if (doRight || doLeft) {
-            let adjCent = Math.max(0, adjRight-adjLeft);
-            let cenStyle = `${adjCent}%`;
-            renderer.setElementStyle(this.center.nativeElement, 'width', cenStyle);
-            HTML.dispatchResizeEvent();
         }
     }
     
