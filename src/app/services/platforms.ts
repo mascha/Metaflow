afac3136cc49098febfde492f65e9b03c79af0d9
triLithium <noreply@reply.not>
2016-05-10 15:57:59 +0200

    Added leftbar; Flat design

--- src/app/services/platforms.ts
+++ src/app/services/platforms.ts
@@ -1,10 +1,20 @@
 import {Injectable} from '@angular/core';
 import {IPlatformLayer} from "../common/platform";
 import {PixiLayer} from "../common/platform/pixi";
-import {KonvaLayer} from "../common/platform/konva";
 
+/**
+ * Service, which provides the underlying rendering platform.
+ * @author Martin Schsfr
+ */
 @Injectable()
 export class PlatformService {
+
+    /**
+     * Retrieve a newly initialized rendering layer, which
+     * will be added to the DOM.
+     * @param element A html canvas element. Currently only canvas is supported.
+     * @returns {IPlatformLayer}
+     */
     getPlatform(element: HTMLCanvasElement): IPlatformLayer {
         return new PixiLayer(element);
     }
