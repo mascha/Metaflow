dd074ca692ff7a4c07c1854a72795c560f3b4235
triLithium <noreply@reply.not>
2016-05-15 17:16:55 +0200

    Simplified sidebar styling and tempalte

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,5 +1,10 @@
 import {Component, Input, EventEmitter, Output} from '@angular/core';
 
+/**
+ * A pane with a title and content.
+ * @author Martin Schade
+ * @since 1.0.0
+ */
 @Component({
     selector: 'sidebar',
     template: require('./sidebar.html'),
