669caf46c71bd154d28b0d548f6ea5abcbfe9dcf
triLithium <noreply@reply.not>
2016-05-19 11:26:24 +0200

    Added hover interaction

--- src/app/components/toolwindows/palette/palette.ts
+++ src/app/components/toolwindows/palette/palette.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, ElementRef, Renderer} from '@angular/core';
+import {Component, ViewChild, ElementRef, Renderer, HostListener} from '@angular/core';
 import PaletteRegistry from "../../../services/palettes";
 import HTML from "../../../common/HTML";
 
@@ -18,41 +18,78 @@ export default class Palette {
     selected: string;
     dimmed = false;
 
+    private timer: any;
+
     @ViewChild('icons') icons: ElementRef;
     @ViewChild('select') select: ElementRef;
+    @ViewChild('overlay') overlay: ElementRef;
 
-    onIconSelect(event) {
-        let off = HTML.getOffset(this.icons.nativeElement, event);
-        let index = Math.floor(off.y / 32);
-        if (index >= 0 && index < this.categories.length && this.select.nativeElement) {
-            this.renderer.setElementStyle(
-                this.select.nativeElement, 'transform', `translateY(${index * 32}px)`
-            );
-            this.components = this.categories[index].components;
-            this.selected = this.categories[index].name;
+    onSelect(event) {
+        let ele = this.icons.nativeElement;
+        let off = HTML.getOffset(ele, event);
+        let index = this.getIndex(off.y);
+        if (this.isValidIndex(index)) {
+            this.selectItem(index);
+            clearTimeout(this.timer);
+            this.dimmed = false;
         }
     }
-    
-    onEnter() {
-        console.log('entered!')
+
+    onOverlayClick(event) {
+        let ele = this.overlay.nativeElement;
+        let off = HTML.getOffset(ele, event);
+        let index = this.getIndex(off.y);
+        if (!this.isValidIndex(index)) {
+            this.selectItem(index);
+        }
+        this.dimmed = false;
     }
-    
+
+    onEnter() {
+        this.timer = setTimeout(() => {
+            this.dimmed = true;
+        }, 666);
+    }
+
     onLeave() {
-        console.log('leaved!')
+        if (!this.dimmed) {
+            clearTimeout(this.timer)
+        }
+    }
+
+    @HostListener('mouseleave')
+    onHostLeave() {
+        clearTimeout(this.timer);
+        if (this.dimmed) {
+            this.dimmed = false;
+        }
+        return false;
+    }
+
+    private selectItem(index: number) {
+        let category = this.categories[index];
+        this.components = category.components;
+        this.selected = category.name;
+        let e = this.select.nativeElement;
+        if (e) {
+            let value = `translateY(${index * 32}px)`;
+            this.renderer.setElementStyle(
+                e, 'transform', value
+            );
+        }
+    }
+
+    private isValidIndex(index: number) {
+        if (!this.categories) { return false; }
+        return (index >= 0 && index < this.categories.length)
+    }
+
+    private getIndex(y: number) {
+        return Math.floor(y / 32);
     }
     
     constructor(private registry: PaletteRegistry,
                 private renderer: Renderer) {
         this.categories = registry.getCategories();
-
-        /*
-        setTimeout(() => {
-            this.dimmed = true;
-        }, 4000);
-
-        setTimeout(() => {
-            this.dimmed = false;
-        }, 8000);
-        */
     }
 }
--- src/app/components/toolwindows/project/project.ts
+++ src/app/components/toolwindows/project/project.ts
@@ -1,9 +1,7 @@
-import {Component, ViewChild, ElementRef, Renderer} from '@angular/core';
-import PaletteRegistry from "../../../services/palettes";
-import HTML from "../../../common/HTML";
+import {Component} from '@angular/core';
 
 /**
- * A component palette.
+ * A project explorer.
  * @author Martin Schade
  * @since 1.0.0
  */
@@ -13,34 +11,5 @@ import HTML from "../../../common/HTML";
     styles: [require('./project.scss')]
 })
 export default class ProjectExplorer {
-    categories: Array<any>;
-    components: any;
-    dimmed = false;
 
-    @ViewChild('icons') icons: ElementRef;
-    @ViewChild('select') select: ElementRef;
-
-    onIconSelect(event) {
-        let off = HTML.getOffset(this.icons.nativeElement, event);
-        let index = Math.floor(off.y / 32);
-        if (index >= 0 && index < this.categories.length && this.select.nativeElement) {
-            this.renderer.setElementStyle(
-                this.select.nativeElement, 'transform', `translateY(${index * 32}px)`
-            );
-            this.components = this.categories[index].components;
-        }
-    }
-    
-    onEnter() {
-        console.log('entered!')
-    }
-    
-    onLeave() {
-        console.log('leaved!')
-    }
-    
-    constructor(private registry: PaletteRegistry,
-                private renderer: Renderer) {
-        this.categories = registry.getCategories();
-    }
 }
