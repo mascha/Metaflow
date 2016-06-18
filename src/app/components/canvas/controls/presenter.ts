8a7a9a12e165bc9b2a0ad1b50b95fd49ee777971
triLithium <noreply@reply.not>
2016-06-18 18:30:54 +0200

    Changed control layer

--- src/app/components/canvas/controls/presenter.ts
+++ src/app/components/canvas/controls/presenter.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {WorkspaceConfig} from "../../../services/configs";
+import ConfigService from "../../../services/configs";
 
 /**
  * Diagramming overlay tools. 
@@ -11,5 +13,11 @@ import {Component} from '@angular/core';
     template: require('./presenter.html')
 })
 export default class Presenter {
-    active = true;
+    active : boolean;
+    workspace : WorkspaceConfig;
+
+    constructor(config: ConfigService) {
+        this.workspace = config.getWorkspaceConfig();
+        this.active = true;
+    }
 }
