1655c3a7432133e1728753cc6bbaccdee16a93d4
Martin Schade <mail@mail.mail>
2016-07-16 21:57:59 +0200

    Upgrade to rc4

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,12 @@
 import {enableProdMode} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ELEMENT_PROBE_PROVIDERS} from '@angular/platform-browser';
-import {ROUTER_PROVIDERS} from '@angular/router';
 import {HTTP_PROVIDERS} from '@angular/http';
 
 const ENV_PROVIDERS = [];
 if (process.env.ENV === 'build') {
   enableProdMode();
 } else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
+  // ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
 import {App} from './app/app';
@@ -30,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function main() {
   return bootstrap(App, [
       SERVICES,
     ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
     ...ENV_PROVIDERS
   ]).catch(err => console.error(err));
 });
