3c00f2bc3034a041738a13b6b5cf3da98fcd8020
Martin Schade <mail@mail.mail>
2016-07-18 16:51:50 +0200

    Fixed deprecated features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,4 @@
 import {Component} from '@angular/core';
-import {ROUTER_DIRECTIVES} from '@angular/router';
-import {FORM_PROVIDERS} from '@angular/common';
-
 import ConfigService from "./services/configs";
 import {WorkspaceConfig} from "./services/configs";
 import Workspace from "./components/workspace/workspace";
@@ -18,17 +15,16 @@ require('../style/globals.scss');
  */
 @Component({
   selector: 'app',
-  providers: [...FORM_PROVIDERS],
-  directives: [...ROUTER_DIRECTIVES, Workspace, Navigation, Toolbar, Diagram],
+  directives: [Workspace, Navigation, Toolbar, Diagram],
   styles: [require('./app.scss')],
   template: require('./app.html')
 })
 export class App {
 
-  workspace: WorkspaceConfig;
+ private workspace: WorkspaceConfig;
   
-  constructor(config: ConfigService) {
-    this.workspace = config.getWorkspaceConfig();
-    document.title = config.getName();
+constructor(config: ConfigService) {
+   this.workspace = config.getWorkspaceConfig();
+   document.title = config.getName();
   }
 }
