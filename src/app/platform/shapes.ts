c736f3cbd71fba405a65a5c56e2d9a50d7ed4b19
Martin Schade <mail@mail.mail>
2016-08-11 18:35:54 +0200

    Added lazy caching

--- src/app/platform/shapes.ts
+++ src/app/platform/shapes.ts
@@ -9,9 +9,51 @@ import {Style} from '../common/styling';
  */
 export default class ShapeRenderer {
 
-  private cache: any;
+  private images: any;
+  private urls: any;
+ 
+  private cacheShape(style: Style, canvas: HTMLCanvasElement) {
+     canvas.width = 16;
+     canvas.height = 16;
 
-  public renderSimple(style: Style): HTMLCanvasElement {
-      return document.createElement('canvas');
+     canvas.fill = 'cornflowerblue';
+     canvas.fillRect(0,0,16,16);
+  }
+
+  getImage(style: Style): HTMLCanvasElement {
+     if (!style) return style;
+     let image = this.images[style];
+     if (image) {
+       return image;
+     } else {
+       image = document.createElement('canvas');
+       this.cacheShape(style, image);
+       return image;
+     }
+  }
+
+  getUrl(style: Style): string {
+     if (!style) return style;
+     let url = this.urls[style];
+     if (url) {
+       return url;
+     } else {
+       let image = this.getImage(style);
+       url = image.toDataUrl();
+       this.urls[style] = image;
+       return url;
+     }
+  }
+
+  removeStyle(style: Style) {
+    if (!style) return style;
+    let image = this.images[style];
+    if (image) {
+      let url = this.urls[style];
+      if (url) {
+        this.urls[style] = undefined;
+      }
+      this.images[style] = undefined;
+    } 
   }
 }
