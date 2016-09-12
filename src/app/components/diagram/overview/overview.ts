14fa84ba78b4a0e68d2d97e2f8e99ba281bc6ba6
Martin Schade <mail@mail.mail>
2016-09-12 18:12:10 +0200

    Prototypical overview implementation

--- src/app/components/diagram/overview/overview.ts
+++ src/app/components/diagram/overview/overview.ts
@@ -1,34 +1,94 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {ViewGroup, ViewVertex} from '../../../common/viewmodel';
+import {Camera, CameraObserver} from '../../../common/camera';
+import {DiagramLayer} from '../layers';
 
+/**
+ * Birds-eye overview component.
+ * 
+ * @author Martin Schade.
+ * @since 1.0.0
+ */
 @Component({
     selector: 'overview',
-    template: '<canvas #surface></canvas>',
+    template: '<canvas #camera></canvas><canvas #nodes></canvas>',
     styles: [require('./overview.scss')]
 })
-export default class Overview {
-    @ViewChild('surface') canvas: ElementRef;
+export default class Overview implements DiagramLayer, CameraObserver {
+    @ViewChild('camera') private camCanvas: ElementRef;
+    @ViewChild('nodes') private nodeCanvas: ElementRef;
+    private nodes: CanvasRenderingContext2D;
+    private cams: CanvasRenderingContext2D;
+    private camera: Camera;
+    private group: ViewGroup;
+
+    public observe(camera: Camera) {
+        this.camera = camera;
+        camera.attachObserver(this);
+    }
     
-    ngAfterViewInit() {
-        let surface = this.canvas.nativeElement as HTMLCanvasElement;
-        let brush = surface.getContext("2d");
-        
-        surface.width = 128;
-        surface.height = 128;
+    public update(group: ViewGroup) {
+        this.group = group;
+        this.redraw();
+    }
 
-        brush.fillStyle = 'cornflowerblue';
+    public onViewResized() {
+        /* NOP */
+    }
 
-        let i = 25;
-        while (i--) {
-            brush.fillRect(
-                Math.round(2 + Math.random() * 124) + .5,
-                Math.round(2 + Math.random() * 124) + .5,
-                2, 2
-            );
+    public onPanChanged(posX: number, posY: number) {
+        this.redrawCamera();
+    }
+
+    public onZoomChanged(zoom: number) {
+        this.redrawCamera();
+    }
+
+    private ngAfterViewInit() {
+        let canvas1 = (this.nodeCanvas.nativeElement as HTMLCanvasElement);
+        this.nodes = canvas1.getContext('2d');
+        let canvas2 = (this.camCanvas.nativeElement as HTMLCanvasElement);
+        this.cams = canvas2.getContext('2d');
+    }
+
+    private redraw() {
+        let brush = this.nodes;
+        brush.clearRect(-1, -1, 500, 500);
+        brush.fillStyle = 'darkgray';
+        brush.globalAlpha = 1;
+
+        let p = this.group;
+        let c = p ? p.contents : null;
+        let l = c ? c.length : 0;
+        let x = 0, y = 0, w = 0, h = 0, i : ViewVertex = null,
+            mW = p.width, mH = p.height, DIM = 128;
+        while (l--) {
+            i = c[l];
+            x = Math.round(i.left / mW) * DIM;
+            y = Math.round(i.top / mH) * DIM;
+            w = Math.round(i.width / mW) * DIM;
+            h = Math.round(i.height / mH) * DIM;
+            // if (w > 0 && h > 0) 
+            brush.fillRect(x, y, 1, 1);
         }
 
-        brush.fillStyle = 'rgba(0,0,0,0.1)';
-        brush.strokeStyle = 'rgba(0, 0, 0, 0.2)';
-        brush.fillRect(42.5, 25.5, 64.5, 28.5);
-        brush.strokeRect(42.5, 25.5, 64.5, 28.5);
+        this.redrawCamera();
+    }
+
+    private redrawCamera() {
+        if (!this.group) return;
+        let brush = this.cams;
+        brush.clearRect(-1, -1, 500, 500);
+        brush.fillStyle = 'cornflowerblue';
+        brush.globalAlpha = 0.2;
+        let x = this.camera.castRayX(this.camera.cameraX);
+        let y = this.camera.castRayY(this.camera.cameraY);
+        let mW = this.group.width, mH = this.group.height, DIM = 128;
+        brush.fillRect(
+            x / mW * DIM, 
+            y / mH * DIM, 
+            this.camera.projWidth / mW * DIM, 
+            this.camera.projHeight / mH * DIM 
+        );
     }
 }
